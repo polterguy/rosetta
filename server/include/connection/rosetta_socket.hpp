deadf1d295ab652270b5cfd2730a5098b22489ae
Thomas Hansen <phosphorusfive@gmail.com>
2016-06-26 23:11:27 +0200

    More cleaning up
    Cleaning up socket class

--- server/include/connection/rosetta_socket.hpp
+++ server/include/connection/rosetta_socket.hpp
@@ -26,30 +26,32 @@
 
 using namespace boost::asio;
 using boost::system::error_code;
-using namespace rosetta::common;
 
 namespace rosetta {
 namespace server {
 
+// Helper to make code more readable.
+typedef std::function<void (const error_code & error, size_t bytes_read)> socket_callback;
+
 
 /// A common base class, wrapping both SSL sockets and normal sockets.
-/// Simply makes it possible to wrap the standard global functions from boost asio, into two
-/// different implementations. One for SSL and another for plain sockets.
+/// Makes it possible to transparently wrap the standard global functions from boost asio, into two
+/// different implementations; One for SSL, and another for plain sockets.
 class rosetta_socket : public std::enable_shared_from_this<rosetta_socket>, public boost::noncopyable
 {
 public:
 
   /// Reads from socket until match condition is reached.
-  virtual void async_read_until (streambuf & buffer, match_condition & match, std::function<void (const error_code & error, size_t bytes_read)> callback) = 0;
+  virtual void async_read_until (streambuf & buffer, match_condition & match, socket_callback callback) = 0;
 
-  /// Reads from socket until match condition is reached.
-  virtual void async_read (streambuf & buffer, detail::transfer_exactly_t no, std::function<void (const error_code & error, size_t bytes_read)> callback) = 0;
+  /// Reads exactly "no" bytes from socket.
+  virtual void async_read (streambuf & buffer, detail::transfer_exactly_t no, socket_callback callback) = 0;
 
-  /// Writes to socket.
-  virtual void async_write (const_buffers_1 buffer, std::function<void (const error_code & error, size_t bytes_read)> callback) = 0;
+  /// Writes the given const_buffer to socket.
+  virtual void async_write (const_buffers_1 buffer, socket_callback callback) = 0;
 
-  /// Writes to socket.
-  virtual void async_write (mutable_buffers_1 buffer, std::function<void (const error_code & error, size_t bytes_read)> callback) = 0;
+  /// Writes the given mutable_buffer to socket.
+  virtual void async_write (mutable_buffers_1 buffer, socket_callback callback) = 0;
 
   /// Returns remote endpoint for socket.
   virtual ip::tcp::endpoint remote_endpoint () = 0;
@@ -68,7 +70,7 @@ public:
 };
 
 
-/// A plain socket.
+/// A plain socket, with no SSL stream associated with it.
 class rosetta_socket_plain final : public rosetta_socket
 {
 public:
@@ -79,34 +81,31 @@ public:
   { }
 
   /// Reads from socket until match condition is reached.
-  void async_read_until (streambuf & buffer, match_condition & match, std::function<void (const error_code & error, size_t bytes_read)> callback) override
+  void async_read_until (streambuf & buffer, match_condition & match, socket_callback callback) override
   {
     boost::asio::async_read_until (_socket, buffer, match, callback);
   }
 
-  /// Reads from the socket until requirements are met.
-  void async_read (streambuf & buffer, detail::transfer_exactly_t no, std::function<void (const error_code & error, size_t bytes_read)> callback) override
+  /// Reads exactly "no" bytes from socket.
+  void async_read (streambuf & buffer, detail::transfer_exactly_t no, socket_callback callback) override
   {
     boost::asio::async_read (_socket, buffer, no, callback);
   }
 
-  /// Writes to socket.
-  void async_write (const_buffers_1 buffer, std::function<void (const error_code & error, size_t bytes_read)> callback) override
+  /// Writes the given const_buffer to socket.
+  void async_write (const_buffers_1 buffer, socket_callback callback) override
   {
     boost::asio::async_write (_socket, buffer, callback);
   }
 
-  /// Writes to socket.
-  void async_write (mutable_buffers_1 buffer, std::function<void (const error_code & error, size_t bytes_read)> callback) override
+  /// Writes the given mutable_buffer to socket.
+  void async_write (mutable_buffers_1 buffer, socket_callback callback) override
   {
     boost::asio::async_write (_socket, buffer, callback);
   }
 
   /// Returns remote endpoint for socket.
   ip::tcp::endpoint remote_endpoint () override { return _socket.remote_endpoint(); }
-
-  /// Returns socket to caller.
-  ip::tcp::socket & socket() { return _socket; }
 
   /// Shuts down socket.
   void shutdown (socket_base::shutdown_type what, error_code & error) override { _socket.shutdown (what, error); }
@@ -117,12 +116,16 @@ public:
   /// Returns true if socket is open.
   bool is_open() override { return _socket.is_open(); }
 
-  /// Returns false.
+  /// Returns false, since this is not a secure (SSL) socket.
   virtual bool is_secure() const override { return false; }
+
+
+  /// Returns socket to caller.
+  ip::tcp::socket & socket() { return _socket; }
 
 private:
 
-  /// Actual boost asio socket for instance
+  /// Actual boost asio socket for instance.
   ip::tcp::socket _socket;
 };
 
@@ -132,37 +135,34 @@ class rosetta_socket_ssl final : public rosetta_socket
 {
 public:
 
-  /// Constructor initializing ssl stream with io_service and context.
+  /// Constructor initializing SSL stream with io_service and context.
   rosetta_socket_ssl (io_service & service, ssl::context & context)
     : _socket (service, context)
   { }
 
   /// Reads from socket until match condition is reached.
-  void async_read_until (streambuf & buffer, match_condition & match, std::function<void (const error_code & error, size_t bytes_read)> callback) override
+  void async_read_until (streambuf & buffer, match_condition & match, socket_callback callback) override
   {
     boost::asio::async_read_until (_socket, buffer, match, callback);
   }
 
-  /// Reads from the socket until requirements are met.
-  void async_read (streambuf & buffer, detail::transfer_exactly_t no, std::function<void (const error_code & error, size_t bytes_read)> callback) override
+  /// Reads exactly "no" bytes from socket.
+  void async_read (streambuf & buffer, detail::transfer_exactly_t no, socket_callback callback) override
   {
     boost::asio::async_read (_socket, buffer, no, callback);
   }
 
-  /// Writes to socket.
-  void async_write (const_buffers_1 buffer, std::function<void (const error_code & error, size_t bytes_read)> callback) override
+  /// Writes the given const_buffer to socket.
+  void async_write (const_buffers_1 buffer, socket_callback callback) override
   {
     boost::asio::async_write (_socket, buffer, callback);
   }
 
-  /// Writes to socket.
-  void async_write (mutable_buffers_1 buffer, std::function<void (const error_code & error, size_t bytes_read)> callback) override
+  /// Writes the given mutable_buffer to socket.
+  void async_write (mutable_buffers_1 buffer, socket_callback callback) override
   {
     boost::asio::async_write (_socket, buffer, callback);
   }
-
-  /// Returns SSL stream wrapping socket to caller.
-  ssl::stream<ip::tcp::socket> & ssl_stream() { return _socket; }
 
   /// Returns remote endpoint for socket.
   ip::tcp::endpoint remote_endpoint () override { return _socket.lowest_layer().remote_endpoint(); }
@@ -176,8 +176,12 @@ public:
   /// Returns true if socket is open.
   bool is_open() override { return _socket.lowest_layer().is_open(); }
 
-  /// Returns true.
+  /// Returns true, since this is an SSL socket.
   virtual bool is_secure() const override { return true; }
+
+
+  /// Returns SSL stream wrapping socket to caller.
+  ssl::stream<ip::tcp::socket> & ssl_stream() { return _socket; }
 
 private:
 
