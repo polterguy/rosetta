7aa950861ffd110948ce37db384a8c08f9c94cbe
Thomas Hansen <phosphorusfive@gmail.com>
2016-06-30 00:38:10 +0200

    Added stress test
    Running 2.000 requests, in 4 different threads, for different types of
    resources, folders and documents, in both SSL and non-SSL, while timing
    how much time these requests takes in total.

---
+++ run-stress-test.sh
@@ -0,0 +1,1 @@
+time ./stress-test.sh
---
+++ stress-test.sh
@@ -0,0 +1,22 @@
+curl -s "http://localhost:8080?[1-500]" &
+pidlist="$pidlist $!" 
+curl -s "http://localhost:8080/index.html?[1-500]" &
+pidlist="$pidlist $!" 
+curl -s "https://localhost:8081?[1-500]" &
+pidlist="$pidlist $!" 
+curl -s "https://localhost:8081/index.html?[1-500]" &
+pidlist="$pidlist $!" 
+
+for job in $pidlist; do
+  wait $job || let "FAIL+=1"
+done
+
+if [ "$FAIL" == "" ]; then
+  printf "\n\n\n                                --==OK==--\n\n\n"
+  printf "2.000 consecutive jobs, fetching on both SSL, and non-SSL connections, \n"
+  printf "both documents and folders, in 4 consecutive threads,\n"
+  printf "was successfully executed in;\n"
+else
+  printf "\n\nFAIL!\n"
+  printf "Failure count; ($FAIL)"
+fi
